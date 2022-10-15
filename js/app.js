ee45ecea4b73b614b79086bdd0eb70b54d8fa9d0
Rumecollins <urhomovorumecollinsrocoutino@gmail.com>
2022-10-15 09:13:50 -0700

    mode styling
    the styling of light and dark mode.

--- js/app.js
+++ js/app.js
@@ -12,13 +12,46 @@ navToggle.addEventListener("click", () => {
    if (visibility === "false"){
         mobileNav.setAttribute("data-visible","true");
         closeNav.style.display ="block";
-        openNav.style.display ="none";
+        openNav.style.display ="none"
    } else {
        mobileNav.setAttribute("data-visible","false");
        closeNav.style.display ="none";
        openNav.style.display ="block"
    }
 })
+
+let darkMode = document.querySelector(".fa-toggle-off");
+let lightMode = document.querySelector(".fa-toggle-on");
+const modeToggle = document.querySelector(".mode-styling")
+
+modeToggle.addEventListener("click", function(){
+     let visibility = modeToggle.getAttribute("data-visible")
+     if (visibility === "false"){
+          modeToggle.setAttribute("data-visible","true");
+
+          darkMode.style.display ="none";
+          lightMode.style.display ="block"
+
+          document.documentElement.style.setProperty("--color-neutral-100","#fff");
+          document.documentElement.style.setProperty("--color-neutral-900","#111");
+          document.documentElement.style.setProperty("--color-accent-300","rgb(240, 252, 252)");
+
+          
+     } else {
+          modeToggle.setAttribute("data-visible","false");
+
+          darkMode.style.display ="block";
+          lightMode.style.display ="none";
+
+          document.documentElement.style.setProperty("--color-neutral-100","#100");
+          document.documentElement.style.setProperty("--color-neutral-900","#fff");
+          document.documentElement.style.setProperty("--color-accent-300","#111");
+
+     }
+  
+     
+})
+
 
 
 
